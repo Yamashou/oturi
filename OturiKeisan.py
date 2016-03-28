d6b1dd616cef843b70f05319d83421bcd1573dac
Shota Yamamota <1230124fw@gmail.com>
2016-03-28 17:59:54 +0900

    Entry Money

---
+++ OturiKeisan.py
@@ -0,0 +1,23 @@
+#config:utf-8
+
+while True:
+    ManNow = input('現在の10000円札の枚数は？：')
+    if len(ManNow) == 0:
+        break
+    ManNow = int(ManNow)
+    GosenNow = input('現在の5000万円札の枚数は？：')
+    SenNow = input('現在の1000円札の枚数は？：')
+    GohyakuNow = input('現在の500円玉の枚数は？：')
+    HyakuNow = input('現在の100円玉の枚数は？：')
+    GojyuNow = input('現在の50円玉の枚数は？：')
+    JyuNow = input('現在の10円玉の枚数は？：')
+    GoNow = input('現在の5円玉の枚数は？：')
+    ItiNow = input('現在の1円玉の枚数は？：')
+    GosenNow = int(GosenNow)
+    SenNow = int(SenNow)
+    GohyakuNow = int(GohyakuNow)
+    HyakuNow = int(HyakuNow)
+    GojyuNow = int(GojyuNow)
+    JyuNow = int(JyuNow)
+    GoNow = int(GoNow)
+    ItiNow = int(ItiNow)
